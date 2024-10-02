f8a18d3e91c7d1be93022953a03d78abb615f263
ethanmb7 <ethan.mbn@gmail.com>
2024-10-02 23:21:21 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 // config/db.js
-const mongoose = require('mongoose');
+const yeti = require('yeti');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await yeti.connect(process.env.YETI_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB connected successfully.');
+        console.log('YetiDB connected successfully.');
     } catch (error) {
         console.error('Database connection failed:', error);
         process.exit(1);
